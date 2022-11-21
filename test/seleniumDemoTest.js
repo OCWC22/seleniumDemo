d126059c03d5eb4b6b90d67b95f7254f6b07de37
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-11-20 18:33:13 -0800

    chai assertion library

--- test/seleniumDemoTest.js
+++ test/seleniumDemoTest.js
@@ -1,9 +1,15 @@
+//https://www.lambdatest.com/blog/mocha-javascript-tutorial-with-examples-for-selenium-testing/
+//https://github.com/SeleniumHQ/seleniumhq.github.io/blob/trunk/examples/javascript/test/getting_started/firstScript.spec.js
+
 const { By, Builder } = require('selenium-webdriver');
 const { suite } = require('selenium-webdriver/testing');
 const AxeBuilder = require('@axe-core/webdriverjs');
 const assert = require("assert");
 
+
 suite(function(env) {
+    //describe() - mocha - mainly to define creation fo test groups in Mocha, takes 2 arguments, first is the name of the test group, second is the function that contains the test cases
+
     describe('First script', function() {
         let driver;
 
@@ -13,10 +19,12 @@ suite(function(env) {
 
         after(async () => await driver.quit());
 
+        //it() - mocha - write invidivual Mocha test cases, takes 2 arguments, first is the name of the test case, second is the function that contains the test steps
         it('First Selenium script', async function() {
             await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
 
             let title = await driver.getTitle();
+            //assertion library - assert - to verify the expected and actual results
             assert.equal("Web form", title);
 
             await driver.manage().setTimeouts({ implicit: 500 });
